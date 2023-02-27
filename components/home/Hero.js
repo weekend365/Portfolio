7e948aa4e66bdf417f0040cfa490929b463fa8fd
skadngus0323 <112064849+skadngus0323@users.noreply.github.com>
2023-02-27 18:41:19 +0900

    자기소개 수정

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -12,8 +12,14 @@ export default function Hero() {
           Welcome to my Portfolio.
         </h1>
         <p className="mb-8 leading-relaxed">
-          유저 친화적인 웹앱을 만들고자 시간과 노력을 아끼지않고 역량을 키우고
-          있는 신입 프론트엔드 개발자 남우현입니다.
+          유저 친화적인 웹앱을 만들고자 하는 신입 프론트엔드 개발자
+          남우현입니다. 이 포트폴리오는 Next.js와 LottieFiles, Notion Api,
+          TailwindCSS를 활용하여 만들었으며. 근래에는 React로 개인 프로젝트를
+          만들거나 방통대 컴퓨터과학과 3학년 편입생으로서 온라인 수업을 들으며
+          CS 지식을 쌓고 있습니다. 가장 가까운 목표로는 정보처리기사, SQLD
+          자격증 취득과 토익 고득점을 위해 준비하고 있으며 아직 많이 부족하지만
+          더 나은 개발자가 되기 위하여 시간과 노력을 아끼지 않고 역량을
+          키우겠습니다.
         </p>
         <div className="flex justify-center">
           <Link href="/projects">
