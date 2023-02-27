9011a1c1191672918d26ad54695bf6949cd50eae
skadngus0323 <112064849+skadngus0323@users.noreply.github.com>
2023-02-27 17:29:54 +0900

    replace twitter link

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             © 2023 Woohyun —
             <a
-              href="https://twitter.com/knyttneve"
+              href="https://www.instagram.com/_weekend365/"
               className="text-gray-600 ml-1"
               rel="noopener noreferrer"
               target="_blank"
